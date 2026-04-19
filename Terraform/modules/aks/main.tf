a52953b572c5c2a4b26cf84255f51dc64591622a
Maksm4 <maks.moszynski@gmail.com>
2026-04-20 00:28:40 +0200

    public cluster

--- Terraform/modules/aks/main.tf
+++ Terraform/modules/aks/main.tf
@@ -33,7 +33,7 @@ resource "azurerm_kubernetes_cluster" "main" {
   location                = azurerm_resource_group.main.location
   resource_group_name     = azurerm_resource_group.main.name
   dns_prefix              = local.prefix
-  private_cluster_enabled = true
+  private_cluster_enabled = false
 
   default_node_pool {
     name           = "default"
